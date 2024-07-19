138bfe0fa3e307e786f7ae26f11353218a250130
Hojin Jang <23309485+jhojin7@users.noreply.github.com>
2024-07-19 12:39:20 +0900

    chore: cleanup popup.html

---
+++ popup.js
@@ -0,0 +1,7 @@
+let app = document.querySelector("#app");
+app.innerHTML = "asdfihlsahdfl";
+let pp = document.createElement("p");
+pp.innerHTML = "hello world";
+console.log(pp);
+app.appendChild(pp);
+console.log(app);
